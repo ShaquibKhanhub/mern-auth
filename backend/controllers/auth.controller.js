179f4b8f9275db259f66eb1e7690cbe260777969
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-12-25 13:41:27 +0530

    feat-add: login & logout functionality.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -10,7 +10,7 @@ export const signup = async (req, res) => {
       throw new Error("All fields are required");
     }
     const existingUser = await User.findOne({ email });
-    console.log("user", existingUser);
+
     if (existingUser) {
       return res
         .status(403)
@@ -21,6 +21,7 @@ export const signup = async (req, res) => {
     const verificationToken = Math.floor(
       100000 + Math.random() * 900000
     ).toString();
+
     const user = new User({
       email,
       password: hashedPassword,
@@ -48,12 +49,78 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
   try {
-  } catch (error) {}
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired verification code",
+      });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+
+    res.status(200).json({
+      success: true,
+      message: "Email is verified successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("error in verifyEmail ", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid email" });
+    }
+    const isValidPassword = await bcryptjs.compare(password, user.password);
+    if (!isValidPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid password" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    user.lastLogin = Date.now();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("error in login ", error);
+    return res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
-  try {
-  } catch (error) {}
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
+
+export const forgotPassword = async (req, res) => {};
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -22,3 +22,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "57f2f9a2-8306-4fe7-8b0d-cf22a16d4718",
+      template_variables: {
+        company_info_name: "ShaquibKhanHub Inc.",
+        name: name,
+      },
+    });
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
